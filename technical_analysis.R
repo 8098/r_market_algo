d6ebc546cd58a17b29c42b78cb8c6c2c3778e69b
Justin Campana <justin.campana@gmail.com>
2016-11-22 20:03:16 -0800

    various updates

--- technical_analysis.R
+++ technical_analysis.R
@@ -5,14 +5,18 @@ colnames(data) <- c('Timestamp', 'Open', 'High', 'Low', 'Close')
 EMA <- EMA(data[,('Close')], 10)
 RSI <- RSI(data[,('Close')], 20)
 SlowRSI <- EMA(RSI(data[,('Close')], 20), 10)
+MACD <- MACD(data[,('Close')], 10, 20, 10, percent = FALSE)
 
-data = data.frame(data, EMA, RSI, SlowRSI)
-head(data, 50)
+data = data.frame(data, EMA, RSI, SlowRSI, MACD)
+names(data)[names(data) == 'macd'] <- 'MACD'
+data <- subset(data, select = -c(signal) )
+
+tail(data, 50)
 nrow(data)
 
 write.table(
   data,
-  file = "~/Git/marketalgo/data_converted_qc/audusd.csv",
+  file = "~/Git/marketalgo/data_qc_converted_hourly/audusd.csv",
   na = "",
   sep = ",",
   row.names = FALSE
