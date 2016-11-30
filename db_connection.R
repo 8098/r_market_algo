6bea5fc8db949fe116126db2c9213349467cc2ba
slampana <justin.campana@gmail.com>
2016-11-30 15:11:27 -0800

    various updates

--- db_connection.R
+++ db_connection.R
@@ -1,5 +1,10 @@
 require(RODBC)
 
-connection <- odbcDriverConnect("Driver={SQL Server};server=localhost;database=MarketAlgo;uid=sa;pwd=LukeSkywalker!")
-results <- sqlQuery(connection, "SELECT TOP 1000 * FROM [dbo].[ImportQCHourlyConverted]")
-View(results)
+file = "~/Git/marketalgo/test/audusd.csv"
+data = read.csv(file = file, header = FALSE)
+colnames(data) <- c('Timestamp', 'Open', 'High', 'Low', 'Close')
+
+connection <- odbcDriverConnect("Driver={SQL Server};server=138.68.59.249;database=MarketAlgo;uid=sa;pwd=LukeSkywalker!")
+# results <- sqlQuery(connection, "SELECT TOP 1000 * FROM [dbo].[Test]")
+# View(results)
+sqlSave(connection, data, tablename = 'dbo.Test', append = TRUE, rownames = FALSE)
