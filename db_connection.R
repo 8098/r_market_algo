b1d76d5356e7b711034ebaab1b77ff86bb6d9981
slampana <justin.campana@gmail.com>
2016-11-30 16:12:57 -0800

    various updates

--- db_connection.R
+++ db_connection.R
@@ -1,20 +1,24 @@
 require(RODBC)
-require(RSQLServer)
-require(devtools)
-install_github("jmp75/rClr", build_vignettes=TRUE)
-install_github('agstudy/rsqlserver', build_vignettes=TRUE) 
+# require(rsqlserver)
+# require(devtools)
+# install_github("jmp75/rClr", build_vignettes=TRUE)
+# install_github('agstudy/rsqlserver', build_vignettes=TRUE) 
 
-start
+start.time <- proc.time()
+
 file = "~/Git/marketalgo/test/audusd.csv"
 data = read.csv(file = file, header = FALSE)
 colnames(data) <- c('Timestamp', 'Open', 'High', 'Low', 'Close')
 
-# connection <- odbcDriverConnect("Driver={SQL Server};server=138.68.59.249;database=MarketAlgo;uid=sa;pwd=LukeSkywalker!")
+connection <- odbcDriverConnect("Driver={SQL Server};server=138.68.59.249;database=MarketAlgo;uid=sa;pwd=LukeSkywalker!")
 # results <- sqlQuery(connection, "SELECT TOP 1000 * FROM [dbo].[Test]")
 # View(results)
-# sqlSave(connection, data, tablename = 'dbo.Test2', append = TRUE, rownames = FALSE)
+sqlSave(connection, data, tablename = 'dbo.Test2', append = TRUE, rownames = FALSE, fast = TRUE)
 
-driver <- dbDriver("SqlServer")
-connection <- dbConnect(driver, url = "server=138.68.59.249;database=MarketAlgo;uid=sa;pwd=LukeSkywalker!")
-results <- dbSendQuery(connection, "SELECT TOP 100 * FROM dbo.Test")
-View(results)
+# driver <- dbDriver("SqlServer")
+# connection <- dbConnect(driver, url = "server=138.68.59.249;database=MarketAlgo;user=sa;password=LukeSkywalker!")
+# dbBulkCopy(connection, "[dbo].[Test2]", value = data)
+
+end.time <- proc.time()
+duration.time <- end.time - start.time
+print(paste("Process duration (seconds):", duration.time))
